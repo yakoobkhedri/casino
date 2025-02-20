eb35e96a6d731d47dc49d8dfb334c9b8e7aa7d8d
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-02-20 23:07:53 +0330

    commit message

---
+++ src/js/script.js
@@ -0,0 +1,146 @@
+// menu
+
+// let hamIcon = document.getElementById('hamIcon');
+// let mobileMenu = document.querySelector('.mobileMenu');
+// let dropdownBtn = document.getElementById('dropdownBtn');
+
+// hamIcon.addEventListener('click', function () {
+//   mobileMenu.classList.toggle('active');
+// })
+// dropdownBtn.addEventListener('click', function () {
+//   this.querySelector('svg').classList.toggle('active');
+//   this.nextElementSibling.classList.toggle('active');
+// })
+// document.getElementById('closemenu').addEventListener('click', function () {
+//   mobileMenu.classList.remove('active');
+// })
+
+// acordion
+
+let acordionBtn = Array.from(document.getElementsByClassName('acordionBtn'));
+
+acordionBtn.forEach((item) => {
+  item.addEventListener('click', function () {
+    item.nextElementSibling.classList.toggle('active');
+    item.querySelector('svg.arrow').classList.toggle('active');
+  })
+})
+
+// swiper
+var banner = new Swiper(".banner", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  autoplay: true,
+  loop: true,
+});
+var popular = new Swiper(".popular", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centeredSlides: true, // اسلاید وسطی در مرکز باشد
+  autoplay: true,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next', // دکمه بعدی
+    prevEl: '.swiper-button-prev', // دکمه قبلی
+  },
+});
+var blog = new Swiper(".blog", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centeredSlides: true, // اسلاید وسطی در مرکز باشد
+  autoplay: true,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next', // دکمه بعدی
+    prevEl: '.swiper-button-prev', // دکمه قبلی
+},
+});
+// select size
+
+let size = Array.from(document.getElementsByClassName('size'));
+
+size.forEach((item) => {
+  item.addEventListener('click', function () {
+    size.forEach((items) => { items.classList.remove('active') });
+    item.classList.add('active');
+  })
+})
+
+// tabs
+
+
+let tabs = Array.from(document.querySelectorAll('.tabBox a'));
+
+tabs.forEach((item) => {
+  item.addEventListener('click', function () {
+    tabs.forEach((items) => { items.classList.remove('active') });
+    item.classList.add('active');
+  })
+})
+
+// input number
+
+let increment = Array.from(document.getElementsByClassName('increment'));
+let decrement = Array.from(document.getElementsByClassName('decrement'));
+
+increment.forEach((item) => {
+  item.addEventListener('click', function () {
+    item.previousElementSibling.value++;
+  })
+});
+decrement.forEach((item) => {
+  item.addEventListener('click', function () {
+    if (item.nextElementSibling.value > 1) {
+      item.nextElementSibling.value--;
+    }
+  })
+});
+
+
+// checkbox
+
+let checkbox = Array.from(document.getElementsByClassName('checkbox'));
+let checkboxRow = Array.from(document.getElementsByClassName('checkboxRow'));
+let dateCheckbox = Array.from(document.getElementsByClassName('dateCheckbox'));
+
+document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
+  checkbox.checked = false; // غیرفعال کردن حالت checked
+});
+
+checkboxRow.forEach((item) => {
+  item.querySelector('input').addEventListener('click', function () {
+    item.querySelector('.checkbox').classList.toggle('active');
+  })
+})
+dateCheckbox.forEach((item) => {
+  item.addEventListener('click', function () {
+    item.classList.toggle('active');
+  })
+})
+// تنظیم مقدار ۰ برای تمام فیلدهای عددی
+document.addEventListener('DOMContentLoaded', function () {
+  const numberInputs = document.querySelectorAll('input[type="number"]');
+  const otpInput = Array.from(document.getElementsByClassName('otpInput'));
+  numberInputs.forEach(input => {
+    input.value = '0';
+  });
+  otpInput.forEach(input2 => {
+    input2.value = '';
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary':'#B13AB7',
+        'secondary':'#0222C3',
+        'dark': '#2C2C2C',
+        'gray': '#CCCCCC'
+      },
+      zIndex: {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+      },
+      padding:{
+        'custom':'calc((100% - 1256px) / 2)'
+       },
+      fontFamily: {
+        paydaRegular: "payda-Regular",
+        paydaMedium: "payda-Medium",
+        paydaSemiBold: "payda-SemiBold",
+        paydaBold: "payda-Bold",
+        paydaExtraBold: "payda-ExtraBold",
+        paydaBlack: "payda-Black",
+        MontserratRegular: "Montserrat-Regular",
+        MontserratBold: "Montserrat-Bold",
+        faNumRegular: "fanumber-Regular",
+        faNumBold: "fanumber-Bold",
+      },
+      maxWidth:{
+        'content':'1456px',
+      },
+    }
+},
+}
+
